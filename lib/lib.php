1441363e0ff423bbe4c250511d88c43488810fbb
Wei-Hao Wang <wwang03@risd.edu>
2020-03-23 14:11:40 -0400

    1. Make the menu nested. The new function nav_next() is in lib/lib.php. Now the layers has 5px padding from each other.

--- lib/lib.php
+++ lib/lib.php
@@ -14,4 +14,85 @@ function get_cookie($name)
 	else
 		return null;
 }
+
+
+function nav_nest($oo, $ids, $root=0)
+{
+	$nav = array();
+	$pass = true;
+	
+	$top = $oo->children_ids_nav($root_id);
+	$root_index = array_search($root_id, $ids);
+	if($root_index === FALSE)
+		$root_index = 0;
+	else
+		$root_index++;
+	
+	// every item that locates before kids, including the current page;
+	$nav_preThis = array();
+
+	// every item after kids;
+	$nav_postThis = array();
+
+	foreach($top as $t)
+	{	
+		$o = $oo->get($t);
+		$d = $root+1;
+		$urls = array($o['url']);
+		$url = implode("/", $urls);			
+		$nav[] = array('depth'=>$d, 'o'=>$o, 'url'=>$url);
+		
+		if($pass && $t == $ids[$root_index])
+		{
+			$pass = false; // short-circuit if statement
+
+			$kids = $oo->children_ids_nav(end($ids));
+			if(empty($kids) && count($ids) > 1)
+			{
+				$kids = $oo->children_ids_nav($ids[count($ids)-2]);
+				array_pop($ids); // leaf is included in siblings
+			}
+			for($i = 0 ; $i < count($ids)-1 ; $i++){
+				// Starting from _en, _es;
+				// count-1 is because there's kids;
+				// when i = 0, d = 2;
+				$thisRoot_url = implode("/", $urls);
+				$d++;
+				
+				// getting all the children;
+				$thisChildren = $oo->children_ids_nav($ids[$i]);
+
+				// The child that is viewed;
+				$theRightChild = $ids[$i+1];
+				$isPre = true;
+				foreach ($thisChildren as $key => $tc) {
+					$o = $oo->get($tc);
+					if($tc == $theRightChild){
+						$urls[] = $o['url'];
+						$url = implode("/", $urls);
+						array_push($nav_preThis, array('depth'=>$d, 'o'=>$o, 'url'=>$url));
+						$isPre = false;
+					}else if($isPre){
+						$url = $thisRoot_url.$o['url'];
+						array_push($nav_preThis, array('depth'=>$d, 'o'=>$o, 'url'=>$url));
+					}else{
+						$url = $thisRoot_url.$o['url'];
+						array_unshift($nav_postThis, array('depth'=>$d, 'o'=>$o, 'url'=>$url));
+					}
+				}
+			}
+
+			$d++;
+			foreach($kids as $k)
+			{	
+				$o = $oo->get($k);
+				$urls[] = $o['url'];
+				$url = implode("/", $urls);
+				$nav[] = array('depth'=>$d, 'o'=>$o, 'url'=>$url);
+				array_pop($urls);
+			}
+		}
+	}
+	return array($nav_preThis, $nav, $nav_postThis);
+}
 ?>
--- views/menu.php
+++ views/menu.php
@@ -4,7 +4,21 @@
     adapted from http://www.o-r-g.com
 */
 $body = $item['body'];  // hide/show
-$nav = $oo->nav($uu->ids);
+
+$nav_nest = nav_nest($oo, $uu->ids);
+
+// 3/23 
+// nav_pre = every item appears above $kids in the menu, 
+// including the current level.
+$nav_pre = $nav_nest[0];
+// 3/23 
+// nav = $kids
+$nav = $nav_nest[1];
+// 3/23
+// nav_pre = every item appears below $kids in the menu, 
+$nav_post = $nav_nest[2];
+
+
 // $traverse = $oo->traverse($item);
 // $nav = $oo->nav_full($traverse);
 
@@ -12,13 +26,32 @@ if($uu->id) {
     ?><div id='home'><a href="/">Materia Abierta</a></div><?
 }
 ?><div id='menu' class='centered centeralign <?= ($body) ? "hidden" : ""; ?>'>
+
     <ul class="nav-level"><?
         $prevd = $nav[0]['depth'];
-        foreach($nav as $n) {
-            $d = $n['depth'];
+        foreach($nav_pre as $np) {
+            $d = $np['depth'];
             if($d > $prevd){
                 ?><ul class="nav-level"><?
             }
+            if(substr($np['o']['name1'],0,1) != '_') {
+                ?><li><?
+                    if($np['o']['id'] != $uu->id) {
+                        if(array_search($np['o']['id'], $uu->ids)){
+                            ?><a class='active menu_ancestor' href='<?= '/' . $np["url"]; ?>'><?= $np['o']['name1']; ?></a><?
+                        } else {
+                            ?><a class='active' href='<?= '/' . $np["url"]; ?>'><?= $np['o']['name1']; ?></a><?
+                        }
+                    } else {
+                        ?><span class='static'><?= $np['o']['name1']; ?></span><?
+                    }
+                ?></li><?
+            }
+            $prevd = $d;
+        }?>
+        <ul id = 'menu_next' class = "nav-level">
+        <?
+        foreach ($nav as $n) {
             if(substr($n['o']['name1'],0,1) != '_') {
                 ?><li><?
                     if($n['o']['id'] != $uu->id) {
@@ -28,9 +61,26 @@ if($uu->id) {
                     }
                 ?></li><?
             }
-            $prevd = $d;
         }
-        ?></ul>
+        ?>
+        </ul>
+        <?
+        foreach($nav_post as $np) {
+            $d = $np['depth'];
+            if($d < $prevd){
+                ?></ul><?
+            }
+            if(substr($np['o']['name1'],0,1) != '_') {
+                ?><li><?
+                    if($np['o']['id'] != $uu->id) {
+                        ?><a class='active' href='<?= '/' . $np["url"]; ?>'><?= $np['o']['name1']; ?></a><?
+                    } else {
+                        ?><span class='static'><?= $np['o']['name1']; ?></span><?
+                    }
+                ?></li><?
+            }
+            $prevd = $d;
+        }?>
     </ul>
 </div>    
 <div id='xx' class='<?= ($body) ? "" : "hidden"; ?>'>
