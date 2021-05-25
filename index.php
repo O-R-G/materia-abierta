f76022b43e28b08ce33537923c57e96372daff76
Wei-Hao Wang <wwang03@risd.edu>
2021-05-25 16:21:05 +0800

    add geImageColor.php

--- index.php
+++ index.php
@@ -4,9 +4,15 @@ $requestclean = strtok($request,"?");
 $uri = explode('/', $requestclean);
 
 require_once('views/head.php');
-require_once('views/menu.php');
-require_once('views/home.php');
-require_once('views/language.php');
-require_once('views/clock.php');
+if($uri[1] == 'getImageColor')
+	require_once('views/getImageColor.php');
+else
+{
+	require_once('views/menu.php');
+	require_once('views/home.php');
+	require_once('views/language.php');
+	require_once('views/clock.php');
+}
+
 require_once('views/foot.php');
 ?>
